b1f63a9050a47c0c629ee048232f27980fd224f0
Diego Andrés Ramírez Cabrera <42146834+diegoturcoramirez1298@users.noreply.github.com>
2018-08-26 15:43:43 -0500

    Create Findtheminmaxlenandaveragevalues.py

---
+++ Findtheminmaxlenandaveragevalues.py
@@ -0,0 +1,3 @@
+Create a function that takes a list of numbers and returns the following statistics:
+def minMaxLengthAverage(arr):
+    return [min(arr), max(arr), len(arr), sum(arr)/float(len(arr))]
