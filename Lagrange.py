5fee2d72f44636bb08e24f2d733616fe111e3443
Diego Andrés Ramírez Cabrera <42146834+diegoturcoramirez1298@users.noreply.github.com>
2018-11-11 12:40:26 -0600

    Create Lagrange.py

---
+++ Lagrange.py
@@ -0,0 +1,31 @@
+from sympy import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1950,1960,1970,1980,1990,2000]
+y = [123.5,131.2,150.7,141.7,203.2,240.5]
+
+pL = ''
+for k in range(len(y)):
+    pL += str(y[k]) +'*'
+    Lxk = 1
+    for i in range(len(x)):
+        if (i == k):
+            continue
+        pL += '(x - %f)*'%(x[i])
+        Lxk *= (x[k]-x[i])
+    pL = pL[:-1]
+    pL += '/%f+'%(Lxk)
+pL = pL[:-1]
+
+expr = sympify(pL)
+#expr = expand(expr)
+print(expand(expr))
+plt.plot(x,y,'go')
+
+x2 = np.linspace(1950,2000,2010)
+x = symbols('x')
+y2 = [expr.subs(x,xi) for xi in x2]
+plt.plot(x2,y2)
+plt.grid()
+#Al igual que con el polinomio de interpolación el valor ronda entre 1940 y 1960
